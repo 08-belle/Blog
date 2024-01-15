8d13adcd5e4a67109510c11a5126f74a6f51cfb0
Olivia Jeison <152225033+08-belle@users.noreply.github.com>
2024-01-15 12:39:37 +0700

    Create Author.php

---
+++ Author.php
@@ -0,0 +1,16 @@
+<?php
+
+// Author.php
+class Author {
+    private $name;
+    private $email;
+
+    public function __construct($name, $email) {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    // Getters and setters
+    // ...
+}
+?>
